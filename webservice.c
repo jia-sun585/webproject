5148d290cac3e40bbe8dabff41dfd3f632a1178a
Sun <864902112@qq.com>
2019-12-29 21:36:50 +0800

    function declaration of http_trans

--- webservice.c
+++ webservice.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-//将socket、bind、listen结合成open_listen_sock辅助函数，用于监听等待服务端的请求，返回一个监听描述符
+//open_listen_sock辅助函数，用于监听等待服务端的请求，返回一个监听描述符
 int open_listen__sock(int port);
+void http_trans(int fd);
 int main()
 {
 	return 0;
