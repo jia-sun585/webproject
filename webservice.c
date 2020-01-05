0e7c3553c3cb6bcee8a832e3ef69d45a4c652bfe
Ben <1397529360@qq.com>
2020-01-05 22:13:55 +0800

    Debug(error: ‘SA’ undeclared):add typedef struct sockaddr SA

--- webservice.c
+++ webservice.c
@@ -10,6 +10,7 @@
 #include <pthread.h>
 #include <malloc.h>
 #include "rio.h"
+typedef struct sockaddr SA;
 //open_listen_sock辅助函数，用于监听等待服务端的请求，返回一个监听描述符
 int open_listen_sock(int port);
 //用于http事务处理
