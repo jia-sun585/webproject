f52674550c529334f54d6bb8e69ee4b6e2ccfc57
Ben <1397529360@qq.com>
2020-01-05 22:22:38 +0800

    Debug:add define LISTENQ

--- webservice.c
+++ webservice.c
@@ -10,6 +10,7 @@
 #include <pthread.h>
 #include <malloc.h>
 #include "rio.h"
+#define LISTENQ  1024
 typedef struct sockaddr SA;
 //open_listen_sock辅助函数，用于监听等待服务端的请求，返回一个监听描述符
 int open_listen_sock(int port);
