bf0f3b28d950b0cd811effb40caf5ebe67f4630b
Sun <864902112@qq.com>
2020-01-06 10:00:19 +0800

    debug:modify scanf to sscanf

--- display_get.c
+++ display_get.c
@@ -6,7 +6,7 @@ int main()
     char *buf,*p;
     char content[8192];
     String name,sex,age,native;
-    scanf("name=%s&sex=%s&age=%s&native=%s",&name,&sex,&age,&native);
+    sscanf("name=%s&sex=%s&age=%s&native=%s",&name,&sex,&age,&native);
     sprintf(content,"<h2>欢迎您访问本网站<h2>");
     sprintf(content,"%s您的名字叫%s,性别%s,来自%s,今年%s岁",content,name,sex,native,age);
     printf("Content-length:%d\r\n",strlen(content));
