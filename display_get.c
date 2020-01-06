ea9d9d7e1399d6e60a0d5557b13e19bf0b482377
Sun <864902112@qq.com>
2020-01-06 09:44:40 +0800

    get the element from the uri

--- display_get.c
+++ display_get.c
@@ -3,5 +3,9 @@
 //用于处理introduce_get提交的get请求
 int main()
 {
-
+    char *buf,*p;
+    char content[8192];
+    String name,sex,age,native;
+    scanf("name=%s&sex=%s&age=%s&native=%s",&name,&sex,&age,&native);
+    return 0;
 }
