583c61a95a7fc9733d6cc3b04be846bae2df9fa6
Sun <864902112@qq.com>
2020-01-06 10:24:21 +0800

    debug:modify the bug

--- display_get.c
+++ display_get.c
@@ -8,7 +8,7 @@ int main()
     char *name,*sex,*age,*native;
     sscanf("name=%s&sex=%s&age=%s&native=%s",name,sex,age,native);
     sprintf(content,"<h2>欢迎您访问本网站<h2>");
-    sprintf(content,"%s您的名字叫%s,性别%s,来自%s,今年%s岁",content,name,sex,native,age);
+    sprintf(content,"%s您的名字叫%s,性别%s,来自%s,今年%s岁",*content,*name,*sex,*native,*age);
     printf("Content-length:%d\r\n",strlen(content));
     printf("Content-type:text/html\r\n\r\n");
     printf("%s",content);
