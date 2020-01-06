bf0b096143e0ebb2095ddbfb0afab25bf169ce15
Ben <1397529360@qq.com>
2020-01-06 20:52:24 +0800

    modify add2.c to test POST

--- add2.c
+++ add2.c
@@ -7,11 +7,11 @@ int main(void) {
     char content[8192];
     int n1=0, n2=0;
 
-    /* Extract the two arguments from standard input *//*
-    scanf("num1=%d&num2=%d", &n1, &n2);*/
+    /* Extract the two arguments from standard input */
+    scanf("num1=%d&num2=%d", &n1, &n2);
 
     /* Make the response body */
-    sprintf(content, "Welcome to add.com: ");
+    sprintf(content, "Welcome to add.com: %d,%d",n1,n2);
 //    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
 //    sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
 //            content, n1, n2, n1 + n2);
