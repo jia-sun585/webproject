24d7d19f99e7e9a5c87b2b346cbc7e083ea7721a
Sun <864902112@qq.com>
2020-01-06 13:14:24 +0800

    Debug:repair the bug

--- cal_get.c
+++ cal_get.c
@@ -5,23 +5,22 @@ int main()
 {
     char *buf,*p;
     char content[8192];
-    char cal;
-    int n1,n2,num;
-    scanf("n1=%d&cal=%c&n1=%d",&n1,&cal,&n2);
+    int n1,n2,num,cal;
+    scanf("n1=%d&cal=%d&n1=%d",&n1,&cal,&n2);
     sprintf(content,"<h2>Welcome to the calculation<h2>");
-    if(cal=='+')
+    if(cal==1)
     {
         num=n1+n2;
         sprintf(content,"%sYou select the adder\r\n<p>",content);
         sprintf(content,"%sThe answer is :%d+%d=%d\r\n<p>",content,n1,n2,num);
     }
-    else if(cal=='-')
+    else if(cal==2)
     {
         num=n1-n2;
         sprintf(content,"%sYou select the subtract\r\n<p>",content);
         sprintf(content,"%sThe answer is :%d-%d=%d\r\n<p>",content,n1,n2,num);
     }
-    else if(cal=='*')
+    else if(cal==3)
     {
         num=n1*n2;
         sprintf(content,"%sYou select the multiply\r\n<p>",content);
